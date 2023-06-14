7c48a89aec877d2cf11d0b808506616f654b9d31
Ewerton Augusto <ewerton.santos2210@gmail.com>
2023-06-14 19:38:06 -0300

    create an initial page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import type { AppProps } from "next/app";
+import "../styles/globalStyle.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import GlobalLayout from '../layout/GlobalLayout';
 
 const Home: React.FC = () => {
-  return <h1>EA Personal Website</h1>
+  return (
+    <GlobalLayout>
+      <section style={{
+        width: "100vw",
+        height: "100vh",
+        padding: "16px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignContent: "center",
+        textAlign: "center",
+        boxSizing: "border-box"
+      }}>
+        <img src="/assets/images/ewerton-augusto-logo.svg" alt="Ewerton Augusto" width={200} style={{
+          margin: "16px auto",
+        }} />
+        <h1 style={{
+          fontSize: "2rem"
+        }}>Coming soon</h1>
+        <p style={{
+          margin: "10px 0 16px"
+        }}>Website is under construction</p>
+        <p>
+          Contact: <a href="mailto:ewerton.santos2210@gmail.com">ewerton.santos2210@gmail.com</a>
+        </p>
+      </section>
+    </GlobalLayout>
+  )
 }
 
 export default Home;
