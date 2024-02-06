8ea7705ae2016fa7c7982fd53c15902921ed5d2e
Ewerton Augusto <ewerton.santos2210@gmail.com>
2024-02-06 15:47:25 -0300

    fix build, implement dark mode

--- src/components/darkMode/DarkMode.tsx
+++ src/components/darkMode/DarkMode.tsx
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { IconType } from '../Icons/types';
 
 const DarkMode: React.FC = () => {
-  const [darkMode, setDarkMode] = useState(typeof window !== 'undefined' && (localStorage.theme === 'dark' ||
-    (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)));
+  const [darkMode, setDarkMode] = useState(
+    typeof window !== 'undefined' &&
+      (localStorage.theme === 'dark' ||
+        (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)),
+  );
 
   const [modeLabel, setModeLabel] = useState('Dark mode');
   const [icon, setIcon] = useState(IconType.MOON_SOLID_1);
@@ -11,24 +14,25 @@ const DarkMode: React.FC = () => {
   const toggleDarkMode = () => setDarkMode(!darkMode);
 
   useEffect(() => {
-    if(darkMode) {
-      document.documentElement.classList.add('dark')
-      setModeLabel('Light mode')
-      setIcon(IconType.SUN_SOLID)
-      return
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+      setModeLabel('Light mode');
+      setIcon(IconType.SUN_SOLID);
+      return;
     }
     document.documentElement.classList.remove('dark');
-    setModeLabel('Dark mode')
-    setIcon(IconType.MOON_SOLID_1)
+    setModeLabel('Dark mode');
+    setIcon(IconType.MOON_SOLID_1);
   }, [darkMode]);
 
   return (
-    <button onClick={toggleDarkMode} className="bg-transparent border-none text-black dark:text-white hover:text-primary dark:hover:text-primary" aria-label={modeLabel} title={modeLabel}>
-      {
-        darkMode
-          ? <i className={`${icon} text-[24px]`} />
-          : <i className={`${icon} text-[24px]`} />
-      }
+    <button
+      onClick={toggleDarkMode}
+      className="bg-transparent border-none text-black dark:text-white hover:text-primary dark:hover:text-primary"
+      aria-label={modeLabel}
+      title={modeLabel}
+    >
+      {darkMode ? <i className={`${icon} text-[24px]`} /> : <i className={`${icon} text-[24px]`} />}
     </button>
   );
 };
